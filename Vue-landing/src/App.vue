7ac4e8f1b20f97aa4cc37a99a93cddd010ff8380
fabian <fabiannabi@yahoo.com.mx>
2020-02-24 23:05:07 -0600

    main nav v1 style and layout

---
+++ Vue-landing/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Mh-header></Mh-header>
+    <Mh-cards></Mh-cards>
+    <Mh-map></Mh-map>
+    <Mh-footer></Mh-footer>
+  </div>
+</template>
+
+
+
+
+<script>
+import MHheader from "./components/MH-header.vue";
+import MHcards from "./components/MH-cards.vue";
+import MHmap from "./components/MH-map.vue";
+import MHfooter from "./components/MH-footer.vue";
+
+export default {
+  components: {
+    "Mh-header": MHheader,
+    "Mh-cards": MHcards,
+    "Mh-map": MHmap,
+    "Mh-footer": MHfooter
+  },
+
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ Vue-landing/src/components/MH-header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="header">
+    <div class="header__link">
+      <a href>Weapons</a>
+      <a href>Monsters</a>
+    </div>
+    <button class="header__button--main">Join the hunt</button>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  background-image: url(../assets/mh-title.jpg);
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+}
+.container {
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.header__link {
+  font-size: 1.5em;
+  text-align: center;
+  margin: auto;
+  background-color: #5c5150;
+}
+
+.header__link a {
+  margin: 100px;
+  text-decoration: none;
+  color: white;
+}
+
+.header__button--main {
+  display: flex;
+  padding: 10px;
+  margin-top: 750px;
+  margin-right: auto;
+  margin-left: auto;
+  border-radius: 1.5em;
+  font-size: 1.2em;
+  font-weight: bold;
+  color: rgba(15, 14, 14, 0.781);
+}
+</style>
